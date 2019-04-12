b44ddbe2433c0b3c67f32cd63eb1006ebd77632e
Ray Ellis <rayellis4@gmail.com>
2019-04-12 12:02:28 -0500

    generate Version.js from package.json in build

---
+++ Version.js
@@ -0,0 +1,8 @@
+
+/*
+ * DO NOT EDIT -- generated from package.json
+ */
+
+class Version {
+   static label = 'GE 3.0 alpha'; // string displayed in the upper right-hand corner of the main GE3 web pages
+}
--- build/allGroupExplorer.js
+++ build/allGroupExplorer.js
@@ -5128,22 +5128,9 @@ class DisplayCycleGraph {
 }
 
 /*
-# Version
-
-A simple class containing version information used to document GE releases.  The static`Version.label()`method produces the formatted output displayed in the upper right-hand corner of the [main (group library)](help/rf-um-mainwindow) page, the [group info](help/rf-um-groupwindow) pages, [sheets](help/rf-um-sheetwindow), and [visualizer](help/rf-um-largewindow) pages.
-   
-```js
-*/
+ * DO NOT EDIT -- generated from package.json
+ */
 
 class Version {
-   static label() {
-      return `GE ${Version.major}.${Version.minor} ${Version.suffix}`
-   }
+   static label = 'GE 3.0 alpha'; // string displayed in the upper right-hand corner of the main GE3 web pages
 }
-
-Version.major = '3';
-Version.minor = '0';
-Version.suffix = 'alpha';
-/*
-```
-*/
