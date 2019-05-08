0e69d21ed944ee72aa532e0405a82f7a998a25e7
Nathan Carter <ncarter@bentley.edu>
2019-05-08 09:31:45 +0100

    We are now at 3.0.0-beta!

--- build/allGroupExplorer.js
+++ build/allGroupExplorer.js
@@ -5463,5 +5463,5 @@ class DisplayCycleGraph {
  */
 
 class Version {
-   static label = 'GE 3.0.0-alpha'; // string displayed in the upper right-hand corner of the main GE3 web pages
+   static label = 'GE 3.0.0-beta'; // string displayed in the upper right-hand corner of the main GE3 web pages
 }
