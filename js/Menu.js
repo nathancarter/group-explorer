34e5a098520576f71cda06fd88acfc447199fb30
Ray Ellis <rayellis4@gmail.com>
2019-08-09 23:40:48 -0500

    change input type to require just eventLocation, compatible with both MouseEvents and Touches

--- js/Menu.js
+++ js/Menu.js
@@ -3,7 +3,7 @@
   export default
 */
 class Menu {
-   static setMenuLocations(event /*: JQueryMouseEventObject */, $menu /*: JQuery */) {
+   static setMenuLocations(event /*: eventLocation */, $menu /*: JQuery */) {
       const menuBox = $menu[0].getBoundingClientRect();
       const menuHeight = menuBox.height;
       const windowHeight = 0.99*window.innerHeight;
